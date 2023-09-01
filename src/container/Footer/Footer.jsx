ce6de9558bb7788e30e26c20ec37587fbedecb65
unknown <andrejzeljkovic3@gmail.com>
2023-09-01 08:57:41 +0200

    first commit

---
+++ src/container/Footer/Footer.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import { FooterOverlay, Newsletter } from '../../components';
+import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi';
+import { images } from '../../constants';
+
+import './Footer.css';
+
+const Footer = () => (
+  <div className='app__footer section__padding'>
+    <FooterOverlay />
+    <Newsletter />
+    <div className='app__footer-links'>
+      <div className='app__footer-links_contact'>
+        <h1 className='app__footer-headtext'>Contact Us</h1>
+        <p className='p__opensans'>Banja Luka BB</p>
+        <p className='p__opensans'>+387 65 888 888</p>
+        <p className='p__opensans'>+387 65 999 999</p>
+      </div>
+      <div className='app__footer-links_logo'>
+        <img src={images.gericht} alt='footer_logo' />
+        <p className='p__opensans'>The best way to find yourself is to lose yourself in the service of others</p>
+        <img src={images.spoon} alt='spoon' className='spoon__img' style={{ marginTop: '15px' }} />
+        <div className='app__footer-links_icons '>
+          <FiFacebook />
+          <FiInstagram />
+          <FiTwitter />
+        </div>
+      </div>
+      <div className='app__footer-links_work'>
+        <h1 className='app__footer-headtext'>Working Hours</h1>
+        <p className='p__opensans'>Monday-Friday</p>
+        <p className='p__opensans'>08:00 am - 12:00 pm</p>
+        <p className='p__opensans'>Saturday-Sunday</p>
+        <p className='p__opensans'>09:00 am - 11:00 pm</p>
+      </div>
+    </div>
+    <div className='footer__copyright'>
+      <p className='p__opensans'>2023 Andrej.All Right reserved</p>
+    </div>
+  </div>
+);
+
+export default Footer;
---
+++ src/container/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { images } from '../../constants';
+import { SubHeading } from '../../components';
+import './Header.css';
+
+const Header = () => (
+  <div className='app__header app__wrapper app__section__padding' id='home'>
+    <div className='app__wrapper_info'>
+      <SubHeading title='Chase the new flavour' />
+      <h1 className='app__header-h1'>The key to Fine Dining</h1>
+      <p className='p__opensans' style={{margin:'2rem 0'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris aliquet tortor id nisi cursus hendrerit at in augue.</p>
+      <button type='button' className='custom__button'>Explore Menu</button>
+    </div>
+
+    <div className='app__wrapper_img'>
+      <img src={images.welcome} alt='header img'/>
+    </div>
+  </div>
+);
+
+export default Header;
